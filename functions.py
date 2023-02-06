2e4e6fabcff3a885432f814761bfa32fd5389d8d
Niranjan <niranjansaini504@gmail.com>
2023-02-06 18:03:07 +0530

    functions.py

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "todo.txt"
+
+def get_todos(filepath = FILEPATH):
+    with open(filepath,"r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+
+def write_todos(todos_args,filepath = FILEPATH):
+    with open(filepath,"w") as file_local:
+        file_local.writelines(todos_args)
+
+
+
+
+if __name__ == "__main__" :
+    print("something")
+    print(get_todos())
